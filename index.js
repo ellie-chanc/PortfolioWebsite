cb85d72aa8ffee404269c6c6a6a27b10a7f3420c
ellie-chanc <chanc@bhf.org.uk>
2024-11-18 13:01:30 +0000

    project info show more btn

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const hamburger = document.querySelector("#hamburger");
 const allLink = document.querySelectorAll(".top-nav-link");
+const allProjectInfo = document.querySelectorAll(".project-info");
 
-hamburger.addEventListener("click", activeMenu);
 
-function activeMenu() {
+hamburger.addEventListener("click", toggleMenu);
+
+
+// collapse menu if a link is clicked
+for (let i = 0; i < allLink.length; i++) {
+    allLink[i].addEventListener("click", toggleMenu);
+}
+
+
+for (let i = 0; i < allProjectInfo.length; i++) {
+    if ((allProjectInfo[i].innerText.length) > 50) {
+        let originalProjectInfo = allProjectInfo[i].innerText;
+        allProjectInfo[i].innerText = allProjectInfo[i].innerText.substring(0, 50) + "... ";
+
+        // add a show more button at the end
+        let showMoreBtn = document.createElement("button");
+        showMoreBtn.classList.add("project-show-more");
+        showMoreBtn.innerText = "Show more";
+        showMoreBtn.style.display = "inline-block";
+        allProjectInfo[i].appendChild(showMoreBtn);
+
+        showMoreBtn.addEventListener("click", () => { 
+            allProjectInfo[i].innerText = originalProjectInfo; 
+        });
+    }
+}
+
+
+function toggleMenu() {
     hamburger.classList.toggle("active");
     for (let i = 0; i < allLink.length; i++) {
         allLink[i].classList.toggle("active");
-    }
-
-    const activeLink = document.querySelectorAll(".top-nav-link.active")
-    for (let i = 0; i < activeLink.length; i++) {
-        activeLink[i].addEventListener("click", activeMenu);
     }
 }
