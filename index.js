9b9e1337842cd0158c94a1d27563f191805202ca
ellie-chanc <chanc@bhf.org.uk>
2024-11-26 15:03:13 +0000

    style small-default

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ await getCourse();
 
 const hamburger = document.querySelector("#hamburger");
 const allLink = document.querySelectorAll(".top-nav-link");
-const courseBookmark = document.querySelector("#course-header .bookmark");
+const courseBookmark = document.querySelector(".bookmark-container");
 const contactForm = document.querySelector("#contact-form");
 let numberOfBookmark = 0;
 
@@ -59,13 +59,13 @@ async function getProject() {
         projectCard.classList.add("project-card");
 
         let projectImg = document.createElement("img");
-        projectImg.classList.add("project-img")
+        projectImg.classList.add("project-img", "card-img")
         projectImg.setAttribute("src", projectListObj[i+1]["img"]);
         projectImg.setAttribute("alt", `${projectListObj[i+1]["title"]} Demo`);
         projectCard.appendChild(projectImg);
 
         let projectTitle = document.createElement("h2");
-        projectTitle.classList.add("project-title");
+        projectTitle.classList.add("project-title", "card-title");
         projectTitle.innerText = projectListObj[i+1]["title"];
         projectCard.appendChild(projectTitle);
 
@@ -74,51 +74,25 @@ async function getProject() {
         projectInfo.innerText = projectListObj[i+1]["info"];
         projectCard.appendChild(projectInfo);
 
-        let projectShowMore = document.createElement("button");
-        projectShowMore.classList.add("project-show-more");
-        projectShowMore.classList.add("hide");
-        projectShowMore.innerText = "Show more";
-        projectCard.appendChild(projectShowMore);
-
         // hide part of the info if too long
         if ((projectInfo.innerText.length) > 50) {
             projectInfo.innerText = projectInfo.innerText.substring(0, 50) + "... ";
-            projectShowMore.style.display = "inline-block";
-            projectShowMore.classList.add("hide");
+            let projectShowMore = document.createElement("a");
+            projectShowMore.classList.add("project-show-more", "hide");
+            projectShowMore.innerText = "Show More";
+            projectInfo.appendChild(projectShowMore);
             projectShowMore.addEventListener("click", function (event) { toggleProjectInfo(event, projectInfo, projectListObj[i+1]["info"]); });
         }
         
-
         let projectGithubLink = document.createElement("a");
-        projectGithubLink.classList.add("project-github-link");
+        projectGithubLink.classList.add("project-github-link", "button");
         projectGithubLink.setAttribute("href", projectListObj[i+1]["github-url"]);
         projectGithubLink.setAttribute("target", "_blank");
-        projectGithubLink.style.display = "none";
-        let projectGithubIcon = document.createElement("img");
-        projectGithubIcon.classList.add("project-github-icon")
-        projectGithubIcon.setAttribute("src", "img/github-mark-white.png");
-        projectGithubIcon.setAttribute("alt", "Github Icon");
+        projectGithubLink.innerText = "Go to Github "
+        let projectGithubIcon = document.createElement("i");
+        projectGithubIcon.classList.add("project-github-icon", "fa-brands", "fa-github");
         projectGithubLink.appendChild(projectGithubIcon);
         projectCard.appendChild(projectGithubLink);
-
-        let projectGithubRepoName = document.createElement("p");
-        projectGithubRepoName.classList.add("project-github-repo-name");
-        projectGithubRepoName.style.display = "none";;
-        projectGithubRepoName.innerText = projectListObj[i+1]["github-repo-name"];
-        projectCard.appendChild(projectGithubRepoName);
-
-        let projectGithubCommit = document.createElement("p");
-        projectGithubCommit.classList.add("project-github-commit");
-        projectGithubCommit.style.display = "none";
-        projectGithubCommit.innerText = "Number of commits: " + await GetGithubCommit(projectListObj[i+1]["github-repo-name"]);
-        projectCard.appendChild(projectGithubCommit);
-
-        let projectGithubShowMore = document.createElement("button");
-        projectGithubShowMore.classList.add("project-github-show-more");
-        projectGithubShowMore.innerText = "Show Github details";
-        projectGithubShowMore.classList.add("hide");
-        projectGithubShowMore.addEventListener("click", function (event) { toggleProjectGithubInfo(event, projectGithubRepoName, projectGithubCommit, projectGithubLink); });
-        projectCard.appendChild(projectGithubShowMore);
 
         projectContainer.appendChild(projectCard);
     }
@@ -136,15 +110,18 @@ async function getCourse() {
         courseCard.setAttribute("draggable", "true");
 
         let couseTitle = document.createElement("h2");
-        couseTitle.classList.add("course-title");
+        couseTitle.classList.add("course-title", "class-title");
         couseTitle.innerText = courseListObj[i+1]["title"];
         courseCard.appendChild(couseTitle);
 
         let couseLink = document.createElement("a");
-        couseLink.classList.add("course-link");
+        couseLink.classList.add("course-link", "button");
         couseLink.setAttribute("href", courseListObj[i+1]["url"]);
         couseLink.setAttribute("target", "_blank");
-        couseLink.innerText = "Go to course";
+        couseLink.innerText = "Go to course ";
+        let couseLinkIcon = document.createElement("i");
+        couseLinkIcon.classList.add("course-link-icon", "fa-solid", "fa-arrow-up-right-from-square");
+        couseLink.appendChild(couseLinkIcon);
         courseCard.appendChild(couseLink);
 
         courseCard.addEventListener("dragstart", function (event) {
@@ -166,11 +143,11 @@ function toggleMenu(event) {
 
 function toggleProjectInfo(event, infoElement, originalInfo) {
     if (event.currentTarget.classList.contains("hide")) {
-        infoElement.innerText = originalInfo;
+        infoElement.childNodes[0].nodeValue = originalInfo + " ";
         event.currentTarget.innerText = "Hide";
         event.currentTarget.classList.remove("hide")
     } else {
-        infoElement.innerText = infoElement.innerText.substring(0, 50) + "... ";
+        infoElement.childNodes[0].nodeValue = infoElement.childNodes[0].nodeValue.substring(0, 50) + "... ";
         event.currentTarget.innerText = "Show More";
         event.currentTarget.classList.add("hide");
     }
