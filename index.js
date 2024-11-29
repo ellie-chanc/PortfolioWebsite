0112f92bbb76d632319f7aa93da74ee21ff923c2
ellie-chanc <chanc@bhf.org.uk>
2024-11-29 16:54:21 +0000

    using template strings to create card html

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import { Octokit } from "https://esm.sh/@octokit/core";
 
-
 const octokit = new Octokit({
     auth: ""
 });
 
 
-await getNav();
-await getProject();
-await getCourse();
+await getNavLinks();
+await getProjects();
+await getCourses();
 
 const hamburger = document.querySelector("#hamburger");
 const allLink = document.querySelectorAll(".top-nav-link");
@@ -29,9 +28,11 @@ courseBookmark.addEventListener("drop", function (event) {
     console.log(sessionStorage.getItem(`course-name-${numberOfBookmark}`));
 });
 
+
 hamburger.addEventListener("click", toggleMenu);
 
-async function getNav() {
+
+async function getNavLinks() {
     let navRes = await fetch("data/nav.json");
     let navObj = await navRes.json();
     let topNav = document.querySelector("#top-nav");
@@ -49,107 +50,80 @@ async function getNav() {
 }
 
 
-async function getProject() {
-    let projectListRes = {};
+async function getProjects() {
     let projectListObj = {};
     try {
-        projectListRes = await fetch("data/project.json");
+        let projectListRes = await fetch("data/project.json");
         projectListObj = await projectListRes.json();
     } catch (err) {
         console.log(`Error name: ${err.name}`);
         console.log("Unable to get project details");
-        projectListRes = {};
         projectListObj = {};
     }
 
     let projectContainer = document.querySelector("#project-container");
-
-    for (let i = 0; i < Object.keys(projectListObj).length; i++) {
-        let projectCard = document.createElement("article");
-        projectCard.classList.add("project-card");
-
-        let projectImg = document.createElement("img");
-        projectImg.classList.add("project-img", "card-img")
-        projectImg.setAttribute("src", projectListObj[i+1]["img"]);
-        projectImg.setAttribute("alt", `${projectListObj[i+1]["title"]} Demo`);
-        projectCard.appendChild(projectImg);
-
-        let projectTitle = document.createElement("h2");
-        projectTitle.classList.add("project-title", "card-title");
-        projectTitle.innerText = projectListObj[i+1]["title"];
-        projectCard.appendChild(projectTitle);
-
-        let projectInfo = document.createElement("p");
-        projectInfo.classList.add("project-info");
-        projectInfo.innerText = projectListObj[i+1]["info"];
-        projectCard.appendChild(projectInfo);
-
-        // hide part of the info if too long
-        if ((projectInfo.innerText.length) > 50) {
-            projectInfo.innerText = projectInfo.innerText.substring(0, 50) + "... ";
-            let projectShowMore = document.createElement("a");
-            projectShowMore.classList.add("project-show-more", "hide");
-            projectShowMore.innerText = "Show More";
-            projectInfo.appendChild(projectShowMore);
-            projectShowMore.addEventListener("click", function (event) { toggleProjectInfo(event, projectInfo, projectListObj[i+1]["info"]); });
+    for (let i = 1; i <= Object.keys(projectListObj).length; i++) {
+        let projectImg = `<img class="project-img card-img" src="${projectListObj[i]["img"]}" alt="${projectListObj[i]["title"]} Demo">`;
+        let projectTitle = `<h2 class="project-title card-title">${projectListObj[i]["title"]}</h2>`;
+        let projectInfo = ``;
+        if (projectListObj[i]["info"].length > 50) {
+            projectInfo = `<p class="project-info">${projectListObj[i]["info"].substring(0, 50)}... <a class="project-show-more hide">Show More</a></p>`;
+        } else {
+            projectInfo = `<p class="project-info">${projectListObj[i]["info"]}</p>`;
         }
+        let projectGithubLink = "github-url" in projectListObj[i] ? `<a href="${projectListObj[i]["github-url"]}" target="_blank" class="project-github-link button">Go to Github <i class="project-github-icon fa-brands fa-github"></i></a>` : "";
+        let projectCard = `<article class="project-card">${projectImg + projectTitle + projectInfo + projectGithubLink}</article>`;
         
-        let projectGithubLink = document.createElement("a");
-        projectGithubLink.classList.add("project-github-link", "button");
-        projectGithubLink.setAttribute("href", projectListObj[i+1]["github-url"]);
-        projectGithubLink.setAttribute("target", "_blank");
-        projectGithubLink.innerText = "Go to Github "
-        let projectGithubIcon = document.createElement("i");
-        projectGithubIcon.classList.add("project-github-icon", "fa-brands", "fa-github");
-        projectGithubLink.appendChild(projectGithubIcon);
-        projectCard.appendChild(projectGithubLink);
+        projectContainer.innerHTML += projectCard;
+    }
 
-        projectContainer.appendChild(projectCard);
+    let projectCard = document.querySelectorAll(".project-card");
+    for (let i = 0; i < projectCard.length; i++) {
+        let projectShowMoreBtn = projectCard[i].querySelector(".project-show-more");
+        if (projectShowMoreBtn) {
+            projectShowMoreBtn.addEventListener("click", (event) => {
+                if (event.currentTarget.classList.contains("hide")) {
+                    projectCard[i].querySelector(".project-info").childNodes[0].nodeValue = projectListObj[i+1]["info"] + " ";;
+                    projectShowMoreBtn.innerText = "Hide";
+                    projectShowMoreBtn.classList.remove("hide")
+                } else {
+                    projectCard[i].querySelector(".project-info").childNodes[0].nodeValue = projectListObj[i+1]["info"].substring(0, 50) + "... ";
+                    projectShowMoreBtn.innerText = "Show More";
+                    projectShowMoreBtn.classList.add("hide");
+                }
+            });
+        }
     }
 }
 
 
-async function getCourse() {
-    let courseListRes = {};
+async function getCourses() {
     let courseListObj = {};
     try {
-        courseListRes = await fetch("data/course.json");
+        let courseListRes = await fetch("data/course.json");
         courseListObj = await courseListRes.json();
     } catch (err) {
         console.log(`Error name: ${err.name}`);
         console.log("Unable to get course details");
-        courseListRes = {};
         courseListObj = {};
     }
 
     let courseContainer = document.querySelector("#course-container");
-    
-    for (let i = 0; i < Object.keys(courseListObj).length; i++) {
-        let courseCard = document.createElement("article");
-        courseCard.classList.add("course-card");
-        courseCard.setAttribute("draggable", "true");
-
-        let couseTitle = document.createElement("h2");
-        couseTitle.classList.add("course-title", "class-title");
-        couseTitle.innerText = courseListObj[i+1]["title"];
-        courseCard.appendChild(couseTitle);
-
-        let couseLink = document.createElement("a");
-        couseLink.classList.add("course-link", "button");
-        couseLink.setAttribute("href", courseListObj[i+1]["url"]);
-        couseLink.setAttribute("target", "_blank");
-        couseLink.innerText = "Go to course ";
-        let couseLinkIcon = document.createElement("i");
-        couseLinkIcon.classList.add("course-link-icon", "fa-solid", "fa-arrow-up-right-from-square");
-        couseLink.appendChild(couseLinkIcon);
-        courseCard.appendChild(couseLink);
-
-        courseCard.addEventListener("dragstart", function (event) {
-            event.dataTransfer.setData("text/plain", courseListObj[i+1]["title"]);
-        });
-
-        courseContainer.appendChild(courseCard);
+    for (let i = 1; i <= Object.keys(courseListObj).length; i++) {
+        let courseTitle = `<h2 class="course-title card-title">${courseListObj[i]["title"]}</h2>`;
+        let courseLink = `<a href="${courseListObj[i]["url"]}" target="_blank" class="course-link button">Go to course <i class="course-link-icon fa-solid fa-arrow-up-right-from-square"></i></a>`;
+        let courseCard = `<article class="course-card" draggable="true">${courseTitle + courseLink}</article>`;
+        
+        courseContainer.innerHTML += courseCard;
     }
+}
+
+
+let courseCards = document.querySelectorAll(".course-card");
+for (let i = 0; i < courseCards.length; i++) {
+    courseCards[i].addEventListener("dragstart", function (event) {
+        event.dataTransfer.setData("text/plain", courseCards[i].querySelector(".course-title").innerText);
+    });
 }
 
 
@@ -157,36 +131,6 @@ function toggleMenu(event) {
     hamburger.classList.toggle("active");
     for (let i = 0; i < allLink.length; i++) {
         allLink[i].classList.toggle("active");
-    }
-}
-
-
-function toggleProjectInfo(event, infoElement, originalInfo) {
-    if (event.currentTarget.classList.contains("hide")) {
-        infoElement.childNodes[0].nodeValue = originalInfo + " ";
-        event.currentTarget.innerText = "Hide";
-        event.currentTarget.classList.remove("hide")
-    } else {
-        infoElement.childNodes[0].nodeValue = infoElement.childNodes[0].nodeValue.substring(0, 50) + "... ";
-        event.currentTarget.innerText = "Show More";
-        event.currentTarget.classList.add("hide");
-    }
-}
-
-
-function toggleProjectGithubInfo(event, githubRepoName, githubCommit, githubLink) {
-    if (event.currentTarget.classList.contains("hide")) {
-        githubRepoName.style.display = "block";
-        githubCommit.style.display = "block";
-        githubLink.style.display = "block";
-        event.currentTarget.innerText = "Hide";
-        event.currentTarget.classList.remove("hide")
-    } else {
-        githubRepoName.style.display = "none";
-        githubCommit.style.display = "none";
-        githubLink.style.display = "none";
-        event.currentTarget.innerText = "Show Github details";
-        event.currentTarget.classList.add("hide");
     }
 }
 
